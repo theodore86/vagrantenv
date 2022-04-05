1e71f883f8f258530128ed26067d93661d7c9ae8
Theodoros Georgomanolis <tgeorgomanolis@gmail.com>
2022-04-05 11:51:57 +0300

    Update docker linter wrapper
    - Update docker/lint.sh
      - Add support of darwin OS for linting
    - Update system.sh helpers
      - Add get_ostype function
    - Update .gitignore
      - Add to ignore ruby files, folders
    - Update .rubocop.yml
      - Add to exlude extra folders

--- tools/docker/lint.sh
+++ tools/docker/lint.sh
@@ -9,18 +9,18 @@ source ../helpers/log.sh
 IMAGE="${1:-theodore86/vagrantenv-ci}"
 TAG="${2:-0.0.4}"
 
-_docker_lint() {
+_linter() {
     docker run --platform linux/amd64 \
     --rm \
     -w /app \
     -v "${PWD}:/app" \
-    -e BUNDLE_PATH=/app/
+    -e BUNDLE_PATH=/app/ \
     -e PRE_COMMIT_HOME=/app/.cache \
     -e VAGRANT_HOME=/app/.vagrant.d \
     "${IMAGE}:${TAG}" tox
 }
 
-docker_linter() {
+_linux_linter() {
     local cuid cgid repo
     is_program_installed docker || error 'Docker is required to be installed'
     cuid=$(docker run --platform linux/amd64 --rm ${IMAGE}:${TAG} id -u) || error 'Docker run command failed'
@@ -28,9 +28,23 @@ docker_linter() {
     { cd ../../ && repo="$(basename $PWD)" && \
       cd ../ && sudo chown -R "$cuid:$cgid" "${repo}" 2>/dev/null;
     } || error "Cannot change $repo permissions"
-    cd $repo && _docker_lint
+    cd $repo && _linter
     { cd ../ && sudo chown -R "$(id -un):$(id -gn)" "${repo}" 2> /dev/null; } || \
         error "Cannot restore $repo permissions"
+}
+
+_darwin_linter() {
+    cd ../../ && _linter
+}
+
+docker_linter() {
+    if [[ $(get_ostype) == 'linux' ]]; then
+        _linux_linter
+    elif [[ $(get_ostype) == 'darwin' ]]; then
+        _darwin_linter
+    else
+        error 'Not supported OS type for linting'
+    fi
 }
 
 docker_linter
--- tools/helpers/system.sh
+++ tools/helpers/system.sh
@@ -7,3 +7,20 @@ is_program_installed() {
   command -v "$cmd" >/dev/null && _return=0
   return ${_return}
 }
+
+get_ostype() {
+  local _ostype ostype
+  _ostype=$(uname)
+  case ${_ostype} in
+      Linux)
+        ostype='linux'
+        ;;
+      Darwin)
+        ostype='darwin'
+        ;;
+      *)
+        ostype='unknown'
+        ;;
+  esac
+  echo ${ostype}
+}
