4e130ff47c2d3935ed0b757283c6a5eed73dfaa8
Theodoros Georgomanolis <tgeorgomanolis@gmail.com>
2022-04-05 11:51:57 +0300

    [skip ci] Docker linter for linux/macosx

--- tools/docker/lint.sh
+++ tools/docker/lint.sh
@@ -9,19 +9,26 @@ source ../helpers/log.sh
 IMAGE="${1:-theodore86/vagrantenv-ci}"
 TAG="${2:-0.0.4}"
 
+_docker_lint() {
+    docker run --platform linux/amd64 \
+    --rm \
+    -w /app \
+    -v "${PWD}:/app" \
+    -e BUNDLE_PATH=/app/
+    -e PRE_COMMIT_HOME=/app/.cache \
+    -e VAGRANT_HOME=/app/.vagrant.d \
+    "${IMAGE}:${TAG}" tox
+}
+
 docker_linter() {
     local cuid cgid repo
-    is_program_installed docker || error 'Required to install docker'
+    is_program_installed docker || error 'Docker is required to be installed'
     cuid=$(docker run --platform linux/amd64 --rm ${IMAGE}:${TAG} id -u) || error 'Docker run command failed'
     cgid=$(docker run --platform linux/amd64 --rm ${IMAGE}:${TAG} id -g) || error 'Docker run command failed'
     { cd ../../ && repo="$(basename $PWD)" && \
       cd ../ && sudo chown -R "$cuid:$cgid" "${repo}" 2>/dev/null;
     } || error "Cannot change $repo permissions"
-    cd $repo && \
-    docker run --platform linux/amd64 --rm -w /app -v "${PWD}:/app" \
-      -e PRE_COMMIT_HOME=/app/.cache \
-      -e VAGRANT_HOME=/app/.vagrant.d \
-      "${IMAGE}:${TAG}" tox
+    cd $repo && _docker_lint
     { cd ../ && sudo chown -R "$(id -un):$(id -gn)" "${repo}" 2> /dev/null; } || \
         error "Cannot restore $repo permissions"
 }
