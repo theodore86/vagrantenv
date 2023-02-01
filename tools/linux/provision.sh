f2dc7539175a485e29292a6999db102091d1f1da
Theodoros Georgomanolis <tgeorgomanolis@gmail.com>
2023-02-01 21:07:46 +0200

    Update vagrant to version 2.3.4, plugins
    - Update vagrant version to 2.3.4 in Dockerfile
    - Update vagrant version to 2.3.4 in linux/provision.sh
    - Update vagrant version to 2.3.4 in ansible role
    - Update vagrant plugins in vagrant.yaml
      - vagrant-vbguest == 0.31.0
      - vagrant-goodhosts == 1.1.5

--- tools/linux/provision.sh
+++ tools/linux/provision.sh
@@ -12,14 +12,16 @@ source ${CWD}/../helpers/log.sh
 source ${CWD}/../helpers/system.sh
 # shellcheck source=tools/helpers/pacapt.sh
 source ${CWD}/../helpers/pacapt.sh
+# shellcheck source=tools/helpers/string.sh
+source ${CWD}/../helpers/string.sh
 
-VAGRANT_VERSION='2.2.19'
+
+VAGRANT_VERSION='2.3.4'
 VAGRANT_VIRTUALBOX_VERSION='virtualbox'
 VAGRANT_TMP_DIR='/tmp/downloads'
 VAGRANT_TMP_DEB="${VAGRANT_TMP_DIR}/vagrant_${VAGRANT_VERSION}.deb"
 VAGRANT_TMP_RPM="${VAGRANT_TMP_DIR}/vagrant_${VAGRANT_VERSION}.rpm"
-VAGRANT_URL_DEB="https://releases.hashicorp.com/vagrant/${VAGRANT_VERSION}/vagrant_${VAGRANT_VERSION}_$(uname -m).deb"
-VAGRANT_URL_RPM="https://releases.hashicorp.com/vagrant/${VAGRANT_VERSION}/vagrant_${VAGRANT_VERSION}_$(uname -m).rpm"
+VAGRANT_BASE_URL="https://releases.hashicorp.com/vagrant/${VAGRANT_VERSION}"
 
 
 install_virtualbox() {
@@ -31,13 +33,44 @@ install_virtualbox() {
   error "Fail to install ${VAGRANT_VIRTUALBOX_VERSION}"
 }
 
+_is_vagrant_updated_version() {
+  if [[ $(version "${VAGRANT_VERSION}") -ge $(version "2.3.0") ]]; then
+    return 0
+  else
+    return 1
+  fi
+}
+
+_vagrant_version() {
+  if _is_vagrant_updated_version; then
+    echo "${VAGRANT_VERSION}-1"
+  else
+    echo "${VAGRANT_VERSION}"
+  fi
+}
+
+_vagrant_debian_arch() {
+  local arch
+  arch=$(uname -m)
+  if _is_vagrant_updated_version; then
+    if [[ $arch == "x86_64" ]]; then
+      arch="amd64"
+    fi
+  fi
+  echo "${arch}"
+}
+
 _install_vagrant() {
-  local url package
+  local url package version arch
   if pacman -P |grep -q 'dpkg'; then
-    url=${VAGRANT_URL_DEB}
+    version=$(_vagrant_version)
+    arch=$(_vagrant_debian_arch)
+    url="${VAGRANT_BASE_URL}/vagrant_${version}_${arch}.deb"
     package=${VAGRANT_TMP_DEB}
   else
-    url=${VAGRANT_URL_RPM}
+    version=$(_vagrant_version)
+    arch=$(uname -m)
+    url="${VAGRANT_BASE_URL}/vagrant_${version}_${arch}.rpm"
     package=${VAGRANT_TMP_RPM}
   fi
   mkdir -p "${VAGRANT_TMP_DIR}" && \
