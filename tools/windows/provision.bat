29087c26e520cbc11e2578dbd9c138326346037b
Theodore Georgomanolis <tgeorgomanolis@gmail.com>
2023-02-28 12:40:57 +0200

    Update windows provision.bat
    - Add goto error in case of script failure

--- tools/windows/provision.bat
+++ tools/windows/provision.bat
@@ -11,19 +11,23 @@ pause
 echo Downloading install.ps1 (chocolatey installation script)
 echo .
 pause
-%systemroot%\System32\WindowsPowerShell\v1.0\powershell.exe -NoProfile -ExecutionPolicy Bypass -Command "((new-object net.webclient).DownloadFile('https://chocolatey.org/install.ps1','%DIR%install.ps1'))"
+%systemroot%\System32\WindowsPowerShell\v1.0\powershell.exe -NoProfile -ExecutionPolicy Bypass -Command "((new-object net.webclient).DownloadFile('https://chocolatey.org/install.ps1','%DIR%install.ps1'))" || goto :error
 
 echo Running chocolatey installer (install.ps1)
 echo .
 pause
-%systemroot%\System32\WindowsPowerShell\v1.0\powershell.exe -NoProfile -ExecutionPolicy Bypass -Command "& '%DIR%install.ps1' %*"
+%systemroot%\System32\WindowsPowerShell\v1.0\powershell.exe -NoProfile -ExecutionPolicy Bypass -Command "& '%DIR%install.ps1' %*" || goto :error
 
 echo Installing project dependencies using chocolatey
 echo .
 pause
-choco install -y %DIR%chocolatey.config
+choco install -y %DIR%chocolatey.config || goto :error
 call refreshenv
 
 echo If it is the first time you are provisioning your system, perform an system reboot
 echo .
 pause
+
+:error
+echo Failed with error #%errorlevel%.
+exit /b %errorlevel%
